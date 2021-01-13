970367e20670a764b494c42a03993b2e9e68906d
Keijitadokoro <gccx.kt.0925@icloud.com>
2021-01-13 21:05:21 +0900

    Finish add_basic_info

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
-                             format: { with: VALID_EMAIL_REGEX },
-                             uniqueness: true
+              format: { with: VALID_EMAIL_REGEX },
+              uniqueness: true
   validates :department, length: { in: 2..30 }, allow_blank: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   validates :work_start_time,  presence: true #1/8追加
   validates :work_finish_time,  presence: true #1/8追加
 
- # 渡された文字列のハッシュ値を返します。
+  #渡された文字列のハッシュ値を返します。
   def User.digest(string)
     cost =
       if ActiveModel::SecurePassword.min_cost
@@ -37,12 +37,13 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-     # トークンがダイジェストと一致すればtrueを返します。
+    # トークンがダイジェストと一致すればtrueを返します。
   def authenticated?(remember_token)
-    #ダイジェストが存在しない場合はfalseを返して終了します。
-     return false if remember_digest.nil?
-     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  #ダイジェストが存在しない場合はfalseを返して終了します。
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
   #ユーザーのログイン情報を破棄します。
   def forget
     update_attribute(:remember_digest, nil)
