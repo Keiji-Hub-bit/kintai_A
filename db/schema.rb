d15ff9a4d84a62e4be5a311eccb4bf2eedc00501
Keijitadokoro <gccx.kt.0925@icloud.com>
2021-01-30 21:32:31 +0900

    supreior実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_223632) do
+ActiveRecord::Schema.define(version: 2021_01_30_104407) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -32,11 +32,12 @@ ActiveRecord::Schema.define(version: 2021_01_16_223632) do
     t.string "department"
     t.string "employee_number"
     t.string "card_ID"
-    t.datetime "basic_time", default: "2021-01-07 23:00:00"
-    t.datetime "work_start_time", default: "2021-01-07 23:30:00"
-    t.datetime "work_finish_time", default: "2021-01-08 08:30:00"
+    t.datetime "basic_time", default: "2021-01-29 23:00:00"
+    t.datetime "work_start_time", default: "2021-01-29 23:30:00"
+    t.datetime "work_finish_time", default: "2021-01-30 08:30:00"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.boolean "superior"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "管理者",
+             email: "sample@email.com",
+             password: "password",
+             admin: true)
+User.create!(name: "上長A",
+             email: "sample1@email.com",
+             password: "password",
+             superior: true)
+User.create!(name: "上長B",
+             email: "sample2@email.com",
+             password: "password",
+             superior: true)
+
+4.times do |n|
+  name  = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               )
+end
