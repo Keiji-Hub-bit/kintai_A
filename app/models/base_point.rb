d76075c0801dbc5e09d611644079e5c92bb3a89d
Keijitadokoro <gccx.kt.0925@icloud.com>
2021-02-12 21:20:21 +0900

    管理者基本情報修正

--- app/models/base_point.rb
+++ app/models/base_point.rb
@@ -1,5 +1,5 @@
 class BasePoint < ApplicationRecord
-  validates :base_number,presence: true, uniquness:true, numericality: {only_integer: true, greater_than_or_equal_to: 0} #2/12追加
+  validates :base_number,presence: true, uniqueness: true, numericality: {only_integer: true, greater_than_or_equal_to: 0} #2/12追加
   validates :base_name, presence: true #2/12追加
   validates :base_kind, inclusion: { in: ["出勤", "退勤"]} #2/12追加
 end
