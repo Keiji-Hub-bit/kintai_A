0a6e2bd18f8bd78cfcde87ae630faaa307e9a8d9
Keijitadokoro <gccx.kt.0925@icloud.com>
2021-02-16 00:05:49 +0900

    gem roo(CSV読込重複解除）

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :basic_time,  presence: true #1/8追加
-  #validates :work_time,  presence: true #2/12追加
+  validates :work_time,  presence: true #2/12追加
   validates :work_start_time,  presence: true #1/8追加
   validates :work_finish_time,  presence: true #1/8追加
 
@@ -52,6 +52,8 @@ class User < ApplicationRecord
   end
 
     #importメソッド #2021/02/03追加
+  require 'CSV'
+
   def self.import(file)
     CSV.foreach(file.path, encoding: 'CP932:UTF-8',headers: true) do |row|
       # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
